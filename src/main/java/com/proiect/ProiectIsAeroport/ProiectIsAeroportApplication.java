d193b86d8d9bfef960556b7b1db222306ca1ccc3
AndreiT24 <cikamide665@gmail.com>
2025-05-10 17:12:40 +0300

    ceva smecher

--- src/main/java/com/proiect/ProiectIsAeroport/ProiectIsAeroportApplication.java
+++ src/main/java/com/proiect/ProiectIsAeroport/ProiectIsAeroportApplication.java
@@ -9,6 +9,7 @@ public class ProiectIsAeroportApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProiectIsAeroportApplication.class, args);
 		System.out.println("Hello World!");
+		System.out.println("Ceva smecher");
 	}
 
 }
