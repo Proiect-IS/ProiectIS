2324a5c3c571093209d39d81ff8a6f3fbdae559d
Stan Remus <104161248+StanRemus15@users.noreply.github.com>
2025-05-10 16:49:42 +0300

    Initial commit

---
+++ src/main/java/com/proiect/ProiectIsAeroport/ProiectIsAeroportApplication.java
@@ -0,0 +1,13 @@
+package com.proiect.ProiectIsAeroport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectIsAeroportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectIsAeroportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proiect/ProiectIsAeroport/ProiectIsAeroportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.ProiectIsAeroport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectIsAeroportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
