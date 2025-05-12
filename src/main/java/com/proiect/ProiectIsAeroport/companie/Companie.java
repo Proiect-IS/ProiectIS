2273149b2fa650b19c6c77682b4b946f4391c690
Stan Remus <104161248+StanRemus15@users.noreply.github.com>
2025-05-12 19:46:04 +0300

    carzy23

--- src/main/java/com/proiect/ProiectIsAeroport/companie/Companie.java
+++ src/main/java/com/proiect/ProiectIsAeroport/companie/Companie.java
@@ -9,18 +9,18 @@ import java.util.List;
 public class Companie {
     List<Zbor> zborList = new ArrayList<Zbor>();
 
-    public void Creare(String codCursa, Tip_Zbor tip_zbor, String oras_plecare, String oras_destinatie, int tarifeBusiness,int tarifeClasa1,int tarifeEconomie, Avion avion, boolean esteTurRetur,int discount, String ziua, String ora,String inceput,String sfarsit)
+    public void Creare(String codCursa, Tip_Zbor tip_zbor, String oras_plecare, String oras_destinatie, int pretBusiness,int pretClasa1,int pretEconomie, Model modelAvion,int locuriBusiness,int locuriClasa1,int locuriEconomie, boolean esteTurRetur,int discount, String ziua, String ora,String inceput,String sfarsit)
     {
 
         int discount_final = (esteTurRetur) ? (discount != 0 ? discount:5):0;
         Zbor zbor;
         if(tip_zbor == Tip_Zbor.SEZONIER)
         {
-            zbor=new Zbor_Sezonier(codCursa,tip_zbor,oras_plecare,oras_destinatie,tarifeBusiness,tarifeClasa1,tarifeEconomie,avion,esteTurRetur,discount,ziua,ora,inceput,sfarsit);
+            zbor=new Zbor_Sezonier(codCursa,tip_zbor,oras_plecare,oras_destinatie,pretBusiness,pretClasa1,pretEconomie,modelAvion,locuriBusiness,locuriClasa1,locuriEconomie,esteTurRetur,discount,ziua,ora,inceput,sfarsit);
         }
         else
         {
-            zbor=new Zbor_Regulat(codCursa,tip_zbor,oras_plecare,oras_destinatie,tarifeBusiness,tarifeClasa1,tarifeEconomie,avion,esteTurRetur,discount,ziua,ora);
+            zbor=new Zbor_Regulat(codCursa,tip_zbor,oras_plecare,oras_destinatie,pretBusiness,pretClasa1,pretEconomie,modelAvion,locuriBusiness,locuriClasa1,locuriEconomie,esteTurRetur,discount,ziua,ora);
         }
 
         zbor.setDiscount(discount_final);
@@ -29,9 +29,9 @@ public class Companie {
             String codRetur = codCursa + "-R";
             Zbor zbor_retur;
             if (tip_zbor == Tip_Zbor.SEZONIER) {
-                zbor_retur = new Zbor_Sezonier(codRetur, tip_zbor, oras_plecare, oras_destinatie, tarifeBusiness,tarifeClasa1,tarifeEconomie, avion, true, discount,ziua, ora, inceput, sfarsit);
+                zbor_retur = new Zbor_Sezonier(codRetur, tip_zbor, oras_plecare, oras_destinatie, pretBusiness,pretClasa1,pretEconomie, modelAvion,locuriBusiness,locuriClasa1,locuriEconomie, true, discount,ziua, ora, inceput, sfarsit);
             } else {
-                zbor_retur = new Zbor_Regulat(codCursa,tip_zbor,oras_plecare,oras_destinatie,tarifeBusiness,tarifeClasa1,tarifeEconomie,avion,true,discount,ziua,ora);
+                zbor_retur = new Zbor_Regulat(codCursa,tip_zbor,oras_plecare,oras_destinatie,pretBusiness,pretClasa1,pretEconomie,modelAvion,locuriBusiness,locuriClasa1,locuriEconomie,true,discount,ziua,ora);
             }
             zbor_retur.setDiscount(discount_final);
             zborList.add(zbor_retur);
