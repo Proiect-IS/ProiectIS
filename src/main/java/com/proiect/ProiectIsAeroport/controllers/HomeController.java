1634f6050f12e0de21f537c540cb8b28de7ecd34
Simoc David <simoc.david@gmail.com>
2025-05-12 17:00:13 +0300

    try

--- src/main/java/com/proiect/ProiectIsAeroport/controllers/HomeController.java
+++ src/main/java/com/proiect/ProiectIsAeroport/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.proiect.ProiectIsAeroport.home;
+package com.proiect.ProiectIsAeroport.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/proiect/ProiectIsAeroport/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.proiect.ProiectIsAeroport.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "client_web/login";
+    }
+}
