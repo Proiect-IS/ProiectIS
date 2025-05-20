a8c49f5ff4795790261f57d51fe027ba244e3813
Stan Remus <104161248+StanRemus15@users.noreply.github.com>
2025-05-20 14:44:14 +0300

    carzy23debugjsfak

--- src/main/java/com/proiect/ProiectIsAeroport/controllers/CompanieController.java
+++ src/main/java/com/proiect/ProiectIsAeroport/controllers/CompanieController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -39,6 +40,8 @@ public class CompanieController {
             zboruri = new ArrayList<>();
         }
     }
+
+    @PostMapping("/stergeZbor")
     public void StergereZbor(@RequestParam ("cod_cursa") String cod_cursa){
         zboruri.removeIf(zbor -> zbor.getCod_cursa().equals(cod_cursa));
 
